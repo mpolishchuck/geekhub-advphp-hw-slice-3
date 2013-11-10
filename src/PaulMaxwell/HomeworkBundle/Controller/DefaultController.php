108498950a456f321e228da6d993b764fbc73e58
Maxim Polishchuck <mpolishchuck@gmail.com>
2013-11-10 20:13:40 +0200

    Added Developer Entity and moved author info into it and saparate partial action.

--- src/PaulMaxwell/HomeworkBundle/Controller/DefaultController.php
+++ src/PaulMaxwell/HomeworkBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace PaulMaxwell\HomeworkBundle\Controller;
 
+use PaulMaxwell\HomeworkBundle\Entity\Developer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -19,5 +20,17 @@ class DefaultController extends Controller
     public function contactAction()
     {
         return $this->render('PaulMaxwellHomeworkBundle:Default:contact.html.twig');
+    }
+
+    public function partialAuthorInfoAction($showLabel = false)
+    {
+        $author = new Developer();
+        $author->setName('Maxim Polishchuck');
+        $author->setContactEmail('mpolishchuck@gmail.com');
+
+        return $this->render('PaulMaxwellHomeworkBundle:Default:_author_info.html.twig', array(
+            'author' => $author,
+            'showLabel' => $showLabel,
+        ));
     }
 }
---
+++ src/PaulMaxwell/HomeworkBundle/Entity/Developer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PaulMaxwell\HomeworkBundle\Entity;
+
+class Developer
+{
+    protected $name;
+    protected $contactEmail;
+
+    /**
+     * @param string $contactEmail
+     */
+    public function setContactEmail($contactEmail)
+    {
+        $this->contactEmail = $contactEmail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContactEmail()
+    {
+        return $this->contactEmail;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
