18d7e77c97789eff40233983142da890b7cf690f
Maxim Polishchuck <mpolishchuck@gmail.com>
2013-11-10 19:22:32 +0200

    Created index, about and contact actions

--- src/PaulMaxwell/HomeworkBundle/Controller/DefaultController.php
+++ src/PaulMaxwell/HomeworkBundle/Controller/DefaultController.php
@@ -10,4 +10,14 @@ class DefaultController extends Controller
     {
         return $this->render('PaulMaxwellHomeworkBundle:Default:index.html.twig');
     }
+
+    public function aboutAction()
+    {
+        return $this->render('PaulMaxwellHomeworkBundle:Default:about.html.twig');
+    }
+
+    public function contactAction()
+    {
+        return $this->render('PaulMaxwellHomeworkBundle:Default:contact.html.twig');
+    }
 }
